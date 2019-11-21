ca3db625dedcc0555e74476d44ed79b5dd8058c4
Preethi Suresh <preethisuresh@Preethis-MBP.T-mobile.com>
2019-11-21 14:02:55 -0500

    first commit

---
+++ HomeWork/src/GitFirstTry.java
@@ -0,0 +1,11 @@
+
+public class GitFirstTry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi there");
+		System.out.println("First git try");
+
+	}
+
+}
